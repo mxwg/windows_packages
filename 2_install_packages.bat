00a7cd18ddc775eeced3f334454870105192a8ca
mxwg <mxwg@users.noreply.github.com>
2016-02-25 21:28:06 +0100

    Add bat files

---
+++ 2_install_packages.bat
@@ -0,0 +1,56 @@
+:: Security software
+::choco install -y avastfreeantivirus
+choco install -y avginternetsecurity
+
+:: Browsers
+choco install -y firefox
+choco install -y googlechrome
+choco install -y flashplayerplugin
+
+:: Reader
+choco install -y adobereader
+
+:: Editors
+choco install -y vim
+choco install -y notepadplusplus
+
+:: Media players
+choco install -y vlc
+choco install -y foobar2000
+
+:: Frameworks
+choco install -y jre8
+
+:: Development
+choco install -y gitextensions
+choco install -y kitty
+
+:: File tools
+choco install -y owncloud-client 
+choco install -y winrar
+::choco install -y 7zip.install
+choco install -y truecrypt
+choco install -y spacesniffer
+
+:: Images
+choco install -y irfanview
+choco install -y screenpresso
+
+
+:: Misc tools
+choco install -y imgburn
+choco install -y scansnapmanager
+choco install -y keepassx
+choco install -y skype
+choco install -y steam
+
+:: Games
+choco install -y teamviewer
+
+
+@echo off
+echo .
+echo .
+echo .
+echo Complete. Check output to see if all 'choco install' statements succeeded.
+pause
